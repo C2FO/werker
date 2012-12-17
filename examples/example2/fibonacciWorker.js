02e1bcd3557a8fb2bad22eb8a38fc75b032dbeba
Doug Martin <doug@dougamartin.com>
2012-12-17 00:04:08 -0600

    Docs!

--- examples/example2/fibonacciWorker.js
+++
@@ -1,9 +0,0 @@
-var werker = require("../../index");
-
-function fibonacci(n) {
-    return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-module.exports = werker.worker()
-    .method("fibonacci", fibonacci)
-    .start();
